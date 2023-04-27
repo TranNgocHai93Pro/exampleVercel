aad9d12796d3d8568e2c919b382da98a364bca30
tranngochai93 <tranngochai@MacBook-Pro-cua-tranngochai.local>
2023-04-27 22:40:08 +0700

    update 3

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const db = require("./db")
+// const db =require('./db')
 router.get("/", async (req, res, next) => {
-  return res.status(200).json(db);
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
 });
 
 module.exports = router;
